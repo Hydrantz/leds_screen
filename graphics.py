896ca2ecbf5397e916d5ad5f45e39bdfbc9803f1
Hydrant1 <ido.k.19980@gmail.com>
2022-03-04 10:41:27 +0200

    add first move logic to the game

--- graphics.py
+++ graphics.py
@@ -1,9 +1,25 @@
+from time import sleep
 import board
 import neopixel
 
 LED_NUM = 600
 GPIO = board.D12
 ORDER = neopixel.GRB
+WHITE = 255, 255, 255
+EMPTY = 0, 0, 0
 
 
-# pixels = neopixel.NeoPixel(GPIO, LED_NUM, pixel_order=ORDER, auto_write=False)
+pixels = neopixel.NeoPixel(GPIO, LED_NUM, pixel_order=ORDER, auto_write=False)
+# pixels = [EMPTY]*LED_NUM
+
+def write2board(grid: list):
+    global pixels
+    width = len(grid)
+    height = len(grid[0])
+    for led in range(len(pixels)):
+        col_index = led // height
+        row_index = led % height
+        if col_index % 2 == 1:
+            row_index = height - row_index - 1
+        pixels[led] = grid[col_index][row_index]
+    pixels.show()
--- snake.py
+++ snake.py
@@ -1,7 +1,10 @@
-import keyboard
+import keyboard, graphics
+import copy
 
 HEIGHT = 20
 WIDTH = 30
+ABS_SPEED = 1
+EMPTY_BOARD = [[graphics.EMPTY for y in range(HEIGHT)] for x in range(WIDTH)]
 
 snake_x = int(WIDTH/2)
 snake_y = int(HEIGHT/2)
@@ -9,4 +12,21 @@ snake_speed = (0, 0)
 
 
 while True:
-    print(keyboard.read_key())
+    if keyboard.is_pressed("up"):
+        snake_speed = (1, 0)
+    elif keyboard.is_pressed("down"):
+        snake_speed = (-1, 0)
+    elif keyboard.is_pressed("left"):
+        snake_speed = (0, 1)
+    elif keyboard.is_pressed("right"):
+        snake_speed = (0, -1)
+    snake_x += ABS_SPEED*snake_speed[0]
+    if snake_x >= WIDTH or snake_x <= 0:
+        snake_x = abs(snake_x - WIDTH)
+    if snake_y >= HEIGHT or snake_y <= 0:
+        snake_y = abs(snake_y - HEIGHT)
+    
+    snake_y += ABS_SPEED*snake_speed[1]
+    board = EMPTY_BOARD
+    board[int(snake_x)][int(snake_y)] = graphics.WHITE
+    graphics.write2board(board)

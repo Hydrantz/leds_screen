840413a43c796079b1ac7d4ff4aae9456911fad7
Hydrant <ido.k.19980@gmail.com>
2023-06-05 05:14:52 +0300

    create boot script

--- leds_screen/boot.py
+++ leds_screen/boot.py
@@ -5,27 +5,31 @@ from controller import buttons, lighting as controller_lights, communication as 
 
 comm.reset_connection()
 
-controller_lights.direction(1)
+controller_lights.set_buttons_color_manually(0,0,255, "s")
 buttons.wait_for('select')
 
-controller_lights.direction(2)
+controller_lights.buttons_clear()
+controller_lights.set_buttons_color_manually(0,0,255, "u")
 buttons.wait_for('up')
 
-controller_lights.direction(3)
+controller_lights.buttons_clear()
+controller_lights.set_buttons_color_manually(0,0,255, "d")
 buttons.wait_for('down')
 
-controller_lights.direction(4)
+controller_lights.buttons_clear()
+controller_lights.set_buttons_color_manually(0,0,255, "l")
 buttons.wait_for('left')
 
-controller_lights.direction(5)
+controller_lights.buttons_clear()
+controller_lights.set_buttons_color_manually(0,0,255, "r")
 buttons.wait_for('right')
 
-controller_lights.direction(1)
+controller_lights.buttons_clear()
 
 for i in range (8):
     comm.transmit("s"+" "*i+"8"+" "*(8-i-1))
     comm.transmit("t"+" "*i+"#"+" "*(8-i-1))
-    sleep(1)
+    sleep(0.5)
 
 
 subprocess.run(["sudo","python", "/home/pi/leds_screens/leds_screen/snake.py"])
